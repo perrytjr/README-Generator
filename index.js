4559c21ce8cc39dcffa8b3291d2c7a937b7ed131
Perry Theobald <perrytjr@gmail.com>
2020-08-26 15:51:22 -0700

    inquirer questions were made

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // array of questions for user
-const questions = [
-
-];
+inquirer
+  .prompt([
+        {
+            type: "input",
+            message: "What is your Project Title?",
+            name: "projectTittle"
+        },
+        {
+            type: "input",
+            message: "Description of your application.",
+            name: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "What are the steps required to install your project?",
+            name: "installationInstructions"
+        },
+       
+        {
+            type: "input",
+            message: "Provide information for usage.",
+            name: "usageInformation"
+        },
+        {
+            type: "input",
+            message: "Provide test instructions.",
+            name: "testsInstructions"
+        },
+        {
+            type: "input",
+            message: "Provide test instructions.",
+            name: "testInstructions"
+        },
+        {
+            type: "list",
+            message: "Which license did you use? ",
+            choices: ['MIT', 'GNU',],
+            name: "licenseName"
+        },
+       
+        {
+            type: "input",
+            message: "What is your GitHub user name?",
+            name: "userName"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "emailAddress"
+        }
+       
+        ])
 
 // function to write README file
 function writeToFile(fileName, data) {
